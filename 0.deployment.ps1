abf9e169255c961ddcfbe8ab5b9e2ec2f5bcf180
ChristianAZCloud <christianclements14@gmail.com>
2024-09-06 18:41:03 -0400

    Updated scripts file

--- 0.deployment.ps1
+++ 0.deployment.ps1
@@ -4,3 +4,10 @@ New-AZResourceGroupDeployment -resourcegroupname $rgname -TemplateFile .\0.main.
 
 #Deploys the resources
 New-AZResourceGroupDeployment -resourcegroupname $rgname -TemplateFile .\0.main.bicep  -TemplateParameterFile .\0.parameters-prod.bicepparam 
+
+#Gets Logging Information about a Deployment 
+Get-AzResourceGroupDeploymentOperation -ResourceGroupName $rgname -DeploymentName vNet
+
+az deployment group validate --resource-group $rgname --template-file .\0.main.bicep --template-p
+
+Test-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile .\0.main.bicep -TemplateParameterFile .\0.parameters-prod.bicepparam
